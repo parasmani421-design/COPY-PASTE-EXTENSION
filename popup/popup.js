2b9e11c3e22519a2927f983eda0108a8b12f519a
Parasmani <parasmani421@gmail.com>
2026-03-18 21:42:38 +0530

    Delete popup directory

--- popup/popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("enableBtn").addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-    
-    chrome.tabs.sendMessage(tab.id, {
-        action: "ENABLE"
-    });
-});
